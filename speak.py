689f8a0d74ccaafb83f4c9e9e8be298d6dac10ca
mohdkali <92200890+mohdkali@users.noreply.github.com>
2023-04-20 22:10:09 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,25 +1,57 @@
-# speak function  - two speak function
+########################### Installing reqiuired packages
+from contextlib import contextmanager
+import sys, os
+import subprocess
+imp = ['pyttsx3', 'selenium==4.1.3']
+modules = []
 
-# 1 windows based
-# advantages = fast ,  offline
-# disadvantages = overspeak, less voices
-import  pyttsx3
+for x in imp:
+    try:
+        modules.append(__import__(x))
+        print(f"Found {x}.")
+    except:
+        @contextmanager
+        def suppress_stdout():
+            with open(os.devnull, "w") as devnull:
+                old_stdout = sys.stdout
+                sys.stdout = devnull
+            try:  
+                yield
+            finally:
+                sys.stdout = old_stdout
+        print(f"{x} Not Found")
+        print("Installling required packages.........")
+        with suppress_stdout():
+            for x in imp:
+                def install(package):
+                        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+                install(x)
+        print("Packages installed successfully")
+        print("Intiating the script...")
 
-def speak(Text):
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty("voices")
-    engine.setProperty("voices",voices[0].id)
-    engine.setProperty("rate",170)
-    print("")
-    print(f"you:{Text}.")
-    engine.say(Text)
-    engine.runAndWait()
+    else:
+        print(f"Facing error while installing {x}")
 
 
-# 2 chrome based
 
-# advantages = more voices, over speaking allowed, more clear
-# disadvantages = word limit, slow
+
+#### Windows based
+import pyttsx3
+
+# def Speak(Text):
+#     engine = pyttsx3.init("sapi5")
+#     voices = engine.getProperty("voices")
+#     engine.setProperty('voices', voices[0].id)
+#     engine.setProperty('rate',170)
+#     print("")
+#     print(f"You : {Text}.")
+#     print("")
+#     engine.say(Text)
+#     engine.runAndWait()
+# Speak("Hello sir")
+
+
+######  Chrome based 
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -28,27 +60,26 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 chrome_options = Options()
-chrome_options.add_argument("--log-level=3")
-chrome_options.headless = True
-Path = "database\\chromedriver.exe"
-driver = webdriver.Chrome(Path,options=chrome_options)
+chrome_options.add_argument('--log-level=3')
+chrome_options.headless = False
+Path = "chromedriver.exe"
+driver = webdriver.Chrome(Path, options=chrome_options)
 driver.maximize_window()
 
-website = r"https://ttsmp3.com/text-to-speech/British%20English/"
+website =  r"https://ttsmp3.com/text-to-speech/British%20English/"
 driver.get(website)
-ButtonSelection = Select(driver.find_element(by=By.XPATH,value='/html/body/div[4]/div[2]/form/select'))
+ButtonSelection = Select(driver.find_element(by=By.XPATH, value= '/html/body/div[4]/div[2]/form/select'))
 ButtonSelection.select_by_visible_text('British English / Brian')
 
-
-def Speak(Text):
+def speak(Text):
 
     lengthoftext = len(str(Text))
 
-    if lengthoftext ==0:
+    if lengthoftext==0:
         pass
     else:
-        print(f"Jarvis : {Text}")
         print("")
+        print(f" AI : {Text}")
         Data = str(Text)
         xpathofsec = '/html/body/div[4]/div[2]/form/textarea'
         driver.find_element(By.XPATH,value=xpathofsec).send_keys(Data)
@@ -72,4 +103,3 @@ def Speak(Text):
 
         else:
             sleep(2)
-
