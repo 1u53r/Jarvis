ad52c9fb487976940345c1a0a9ca69d256670f3e
mohdkali <92200890+mohdkali@users.noreply.github.com>
2023-04-28 11:20:25 +0530

    Add files via upload

---
+++ open_youtube.py
@@ -0,0 +1,20 @@
+
+
+import webbrowser
+from speak import Speak
+from body.listen import MicExecution
+
+def Play_youtube():
+
+    text = MicExecution().lower()
+    text1 = text.replace('play',"playing")
+    Speak(text1)
+    api_key = "AIzaSyBbMk2mzaJyuHquRt6C6DlOiuay5OvOqEc"
+    from googleapiclient.discovery import build   
+    text= text.split("play")
+    search_5 = str(text[0: ])
+    youtube = build("youtube" , "v3" , developerKey = api_key)
+    req = youtube.search().list(q = search_5 ,part="id",type="video",fields="items/id") 
+    res = req.execute()
+    urlad = res['items'][0]['id']['videoId']    
+    webbrowser.open("https://www.youtube.com/watch?v=" + urlad)
